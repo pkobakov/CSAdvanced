241ed66f99d42fc9f260db23a1b97105c424a239
Petar Kobakov <pkobakov@gmail.com>
2023-05-11 17:55:04 +0300

    Add New Solution

---
+++ Stack&Queues/Lab/Simple Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = Console.ReadLine().Split();
+var expression = new Stack<string>(input.Reverse());
+
+int result = int.Parse(expression.Pop());
+
+while (expression.Any()) 
+{
+    string sign = expression.Pop();
+    int number = int.Parse(expression.Pop());
+
+    if (sign == "+")
+    {
+        result += number;
+    }
+
+    else if (sign == "-") 
+    {
+        result -= number;
+    }
+}
+
+Console.WriteLine(result);

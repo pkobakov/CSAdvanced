4673e6c417465468a6aa7fb6c63e3906f93ac8ce
Petar Kobakov <pkobakov@gmail.com>
2023-05-12 16:49:39 +0300

    Add Solution

---
+++ Stack&Queues/Lab/Traffic Jam/Program.cs
@@ -0,0 +1,43 @@
+﻿var n = int.Parse(Console.ReadLine());
+var command = Console.ReadLine();
+int count = 0;  
+
+var cars = new Queue<string>();
+while (command.ToLower() != "end") 
+{
+    if (command.ToLower() == "green") 
+    {
+        if (cars.Count < n ) 
+        {
+            while (cars.Count > 0) 
+            {
+
+                count++;
+                Console.WriteLine($"{cars.Dequeue()} passed!");
+            }
+        }
+        else
+        {
+            for(int i = 0; i < n; i ++) 
+            {
+               count ++;   
+               Console.WriteLine($"{cars.Dequeue()} passed!");
+            }
+
+        }
+
+    }
+
+    else
+    {
+        cars.Enqueue(command);
+
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{count} cars passed the crossroads.");
+
+
+

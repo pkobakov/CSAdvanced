2c6253a00f7166e4bae47cbb51117c555f18ab01
Petar Kobakov <pkobakov@gmail.com>
2023-05-12 15:56:56 +0300

    Add New Solution

---
+++ Stack&Queues/Lab/Supermarket/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = Console.ReadLine();
+var customers = new Queue<string>();
+
+while (input != "End")
+{
+    
+    if (input == "Paid")
+	{
+        while (customers.Count > 0) 
+        {
+            Console.WriteLine(  customers.Dequeue());
+        }
+       
+	}
+
+    else
+    {
+        customers.Enqueue(input);
+
+    }
+    
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"{customers.Count} people remaining");

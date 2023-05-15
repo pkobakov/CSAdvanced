8b57c59bbc93d8d19110f3f9a8989c914c7952de
Petar Kobakov <pkobakov@gmail.com>
2023-05-15 23:34:11 +0300

    Added New Solution

---
+++ Stack&Queues/Exercise/Exercise/Fast Food/Program.cs
@@ -0,0 +1,31 @@
+﻿var foodQuantity = int.Parse(Console.ReadLine());
+var ordersQuantity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+var orders = new Queue<int>(ordersQuantity);
+
+Console.WriteLine(orders.Max());
+
+while(foodQuantity > 0)
+{
+	if (orders.Count >  0 && orders.Peek() <= foodQuantity)
+	{
+        foodQuantity -= orders.Dequeue();
+
+	}
+
+	else
+	{
+		break;
+	}
+}
+
+if (orders.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+
+else 
+{
+	var ordersArr = orders.ToArray();
+    Console.WriteLine($"Orders left: {string.Join(" ", ordersArr)}");
+}

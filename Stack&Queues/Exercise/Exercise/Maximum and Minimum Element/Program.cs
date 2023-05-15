034fff3fe7de8d84f091e2e7b2f51e16800d9e2c
Petar Kobakov <pkobakov@gmail.com>
2023-05-15 16:23:57 +0300

    Added New Solution

---
+++ Stack&Queues/Exercise/Exercise/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,38 @@
+﻿var commandsCount = int.Parse(Console.ReadLine());
+var stack = new Stack<int>();
+
+for (int i = 0; i < commandsCount; i++) 
+{
+    var command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    if (command.Length == 2) 
+    {
+        stack.Push(command[1]);
+    }
+
+    else 
+    {
+        var commandType = command[0];
+
+        if (commandType == 2) 
+        {
+            stack.Pop();
+        }
+        if (commandType == 3 && stack.Any()) 
+        {
+            Console.WriteLine(stack.Max());
+        }
+        if(commandType == 4 && stack.Any())
+        {
+            Console.WriteLine(stack.Min());
+        }
+    }
+}
+
+if (stack.Any()) 
+{
+   var result = stack.ToArray();
+   Console.WriteLine( string.Join( ", ", result)); 
+}
+
+

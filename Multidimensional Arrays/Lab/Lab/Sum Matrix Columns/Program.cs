3dbe3ae3a0b82da6a87392074b400e38e96688b4
Petar Kobakov <pkobakov@gmail.com>
2023-05-16 23:38:40 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Lab/Lab/Sum Matrix Columns/Program.cs
@@ -0,0 +1,33 @@
+﻿var dimensions = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+var rows = dimensions[0];
+var columns = dimensions[1];
+
+var matrix = new int[rows, columns];
+
+for (var i = 0; i < rows; i++)
+{
+    var numbers = Console.ReadLine().Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+    
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = numbers[j];
+    }
+}
+
+
+for (var i = 0;i < columns; i++) 
+{
+    int sum = 0;
+    for (int j = 0; j < rows; j++)
+    {
+        sum += matrix[j, i];
+    }
+
+    Console.WriteLine(sum);
+}
+

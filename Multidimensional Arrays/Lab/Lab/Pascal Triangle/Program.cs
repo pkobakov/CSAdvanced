61b43e5a32abcd5eb2baaf19fd15bdc4e325db50
Petar Kobakov <pkobakov@gmail.com>
2023-05-17 19:24:37 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Lab/Lab/Pascal Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿int rows = int.Parse(Console.ReadLine());
+
+long[][] pascalTriangle = new long[rows][];
+if (rows >= 1)
+    pascalTriangle[0] = new long[] { 1 };
+if (rows >= 2)
+{
+    pascalTriangle[1] = new long[] { 1, 1 };
+}
+
+for (int row = 2; row < rows; row++)
+{
+    pascalTriangle[row] = new long[row + 1];
+    pascalTriangle[row][0] = 1;
+
+    for (int col = 1; col < row; col++)
+    {
+        pascalTriangle[row][col] = pascalTriangle[row - 1][col] + pascalTriangle[row - 1][col - 1];
+    }
+
+    pascalTriangle[row][row] = 1;
+
+}
+
+
+foreach (var currentRow in pascalTriangle)
+{
+    Console.WriteLine(string.Join(" ", currentRow));
+}

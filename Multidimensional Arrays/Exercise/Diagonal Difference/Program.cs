66df793c3598ce20aaf8b309e92ae3aa52f21cef
Petar Kobakov <pkobakov@gmail.com>
2023-05-17 20:36:41 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Exercise/Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int rows = size, cols = size;
+
+var square = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    
+    for (int col = 0; col < cols; col++) 
+    {
+        square[row, col] = numbers[col];
+    }
+}
+
+int primary =  0;
+int secondary = 0;
+
+for (int i = 0; i < size; i++) 
+{
+    primary += square[i, i];
+    secondary += square[size-i-1, i];
+
+}
+
+Console.WriteLine(Math.Abs(primary-secondary));

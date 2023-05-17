49d2188359dda6a49fdc6767c9102629f88b8664
Petar Kobakov <pkobakov@gmail.com>
2023-05-17 22:58:49 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Exercise/Squares in Matrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Common;
+
+var dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+int rows = dimensions[0];
+int columns = dimensions[1];    
+
+var matrix = new string[rows, columns];
+int counter = 0;
+
+for (int row = 0; row < rows; row++) 
+{
+    var symbols = Console.ReadLine()
+                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                     .ToArray();
+
+    for (int col = 0; col < columns ; col++)
+    {
+        matrix[row, col] = symbols[col];
+    }
+}
+
+for (int i = 0; i < rows -1 ; i++)
+{
+    for (int j = 0; j < columns - 1 ; j++)
+    {
+        string a = matrix[i, j]; 
+        string b = matrix[i, j + 1];    
+        string c = matrix[i + 1,j] ;
+        string d = matrix[i + 1, j + 1];
+
+        if (a == b && 
+            b == c &&
+            c == d)
+        {
+            counter++;  
+        }
+
+        //else { continue; }
+
+    }
+}
+
+Console.WriteLine(counter);

dc872e999733385c9a94f54aebfdb2b42e5f6c4d
Petar Kobakov <pkobakov@gmail.com>
2023-05-17 13:49:48 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Lab/Lab/Symbol in Matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿int input = int.Parse(Console.ReadLine());
+int rows = input, cols = input;
+
+int[,] matrix = new int[rows,cols];
+
+for (int row = 0; row < rows; row++) 
+{
+    var chars = Console.ReadLine().ToCharArray();;
+
+   for (int col = 0; col < cols; col++) 
+   {
+        matrix[row, col] = chars[col];  
+   }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        if (matrix[row,col] == symbol)
+        {
+            Console.WriteLine(  $"({row}, {col})");
+        }
+    }
+}
+
+Console.WriteLine($"{symbol} does not occur in the matrix");

e6b687c1d6f9164d7996639045a30494e99149f3
Petar Kobakov <pkobakov@gmail.com>
2023-05-18 18:12:33 +0300

    Added New Solution

---
+++ Multidimensional Arrays/Lab/Lab/Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿int row = 3;
+int col = 4;
+
+var matrix = new int [row, col];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+	for (int j = 1; j <= matrix.GetLength(1); j++)
+	{
+        Console.Write(j);
+    }
+    Console.WriteLine();
+}
+
---
+++ Stack&Queues/Exercise/Exercise/Balanced Parentheses/Program.cs
@@ -0,0 +1,45 @@
+﻿
+var paretheses = Console.ReadLine();
+var stack = new Stack<char>();
+
+foreach (char parenthese in paretheses)
+{
+    switch (parenthese) 
+    { 
+        case '(':    
+        case '{':
+        case '[':
+            stack.Push(parenthese);
+        break;
+
+        case ')':
+            if (stack.Count == 0 || stack.Pop() != '(')
+            {
+                Console.WriteLine("NO");
+                return;
+
+            } break;
+
+        case '}':
+            if (stack.Count == 0 || stack.Pop() != '{')
+            {
+                Console.WriteLine("NO");
+                return;
+
+            } break;
+
+        case ']':
+            if (stack.Count == 0 || stack.Pop() != '[')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+    }
+}
+
+if (stack.Count > 0)  Console.WriteLine("NO"); 
+else Console.WriteLine("YES");
+
+
+

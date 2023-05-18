743d0e4ed6f22b5c0c70a8666dc7dd6648062a02
Petar Kobakov <pkobakov@gmail.com>
2023-05-19 00:18:36 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Lab/Average Student Grades/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string,List<decimal>> grades = new Dictionary<string,List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    string name = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (!grades.ContainsKey(name))
+    {
+        grades[name] = new List<decimal> { grade };
+
+    }
+    else 
+    {
+    
+        grades[name].Add(grade);
+    }
+
+}
+
+foreach (var item in grades)
+{
+
+    Console.WriteLine($"{item.Key} -> {string.Join(" ", item.Value.Select(x => $"{x:f2}"))} (avg: {item.Value.Average(x =>x):f2})");
+}

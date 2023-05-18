2c21ceaa06edda6bd2aea2e630d3dc3fcbef5a1b
Petar Kobakov <pkobakov@gmail.com>
2023-05-18 23:25:59 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Lab/Count Same Values in Array/Program.cs
@@ -0,0 +1,20 @@
+﻿double[] inputNumbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+
+Dictionary<double, int> dictionary = new Dictionary<double, int>();
+
+foreach (var number in inputNumbers) 
+{
+    if (!dictionary.ContainsKey(number)) 
+    {
+        dictionary.Add(number, 0);
+    }
+
+    dictionary[number]++; 
+}
+
+foreach (var item in dictionary.Keys) 
+{
+    Console.WriteLine($"{item} - {dictionary[item]} times");
+
+} 

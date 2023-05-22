e287d9bb229a62610b83bab93218378b34c1ebad
Petar Kobakov <pkobakov@gmail.com>
2023-05-22 18:03:49 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Lab/Parking Lot/Program.cs
@@ -0,0 +1,22 @@
+﻿HashSet<string> parkingLot = new HashSet<string>();
+
+string input = Console.ReadLine();
+
+while (input != "END")
+{
+    string[] cmdArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    string action = cmdArgs[0];
+    string carNumber = cmdArgs[1];
+
+    switch (action) 
+    {
+        case "IN": parkingLot.Add(carNumber); break;
+        case "OUT": parkingLot.Remove(carNumber); break;
+            default:throw new ArgumentException("Invalid Car Number"); break;
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (string arg in parkingLot) { Console.WriteLine(arg); }

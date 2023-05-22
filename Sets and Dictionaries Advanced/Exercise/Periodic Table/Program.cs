de3b27ad5bf16af2e8aac0d7722153b51a78479a
Petar Kobakov <pkobakov@gmail.com>
2023-05-23 00:04:37 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Exercise/Periodic Table/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<string> collection = new List <string> ();
+HashSet<string> list = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] inputTokens = Console.ReadLine().Split().ToArray();
+	for (int j = 0; j < inputTokens.Length; j++)
+	{
+		collection.Add(inputTokens[j]);
+	}
+}
+
+foreach (string s in collection.OrderBy(x => x)) 
+{
+     list.Add(s);
+}
+Console.WriteLine(string.Join(" ", list));

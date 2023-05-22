6e4330ab2002a5db9059d8532f332756d9178088
Petar Kobakov <pkobakov@gmail.com>
2023-05-22 18:46:14 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Lab/SoftUni Party/Program.cs
@@ -0,0 +1,55 @@
+﻿string command = Console.ReadLine();
+HashSet<string> regularGuests = new HashSet<string>();
+HashSet<string> VIPguests = new HashSet<string>();
+HashSet<string> partyGuests = new HashSet<string>();
+int counter = 0;    
+
+while (command != "PARTY") 
+{
+	
+	char[] guestCode = command.ToCharArray();
+	char firstSymbol = guestCode[0];
+
+	if (char.IsDigit(firstSymbol)) 
+	{
+		VIPguests.Add(command);
+	}
+
+	else
+	{
+		regularGuests.Add(command);
+	}
+
+    command = Console.ReadLine();   
+}
+
+while (command != "END")
+{
+    char[] guestCode = command.ToCharArray();
+    char firstSymbol = guestCode[0];
+
+    if (char.IsDigit(firstSymbol))
+    {
+        partyGuests.Add(command);
+        VIPguests.Remove(command);
+        
+    }
+
+    else
+    {
+        partyGuests.Add(command);
+        regularGuests.Remove(command);
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(  $"{VIPguests.Count + regularGuests.Count}");
+foreach (string name in VIPguests) 
+{
+    Console.WriteLine(name); 
+}
+foreach (string name in regularGuests)
+{
+    Console.WriteLine(name);
+}
+

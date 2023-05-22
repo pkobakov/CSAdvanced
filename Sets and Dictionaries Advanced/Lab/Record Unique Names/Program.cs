e0dd750012d6c859597df3181333a1d15ddc7917
Petar Kobakov <pkobakov@gmail.com>
2023-05-22 17:31:13 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Lab/Record Unique Names/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    names.Add(name);
+}
+
+Console.WriteLine("-----");
+foreach (string name in names) { Console.WriteLine( name ); }

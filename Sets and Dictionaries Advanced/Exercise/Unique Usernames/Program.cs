20ea5996cd5d4573dd5084176a57a6d069fc118d
Petar Kobakov <pkobakov@gmail.com>
2023-05-22 22:45:29 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Exercise/Unique Usernames/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+HashSet<string> usernames = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string username = Console.ReadLine();
+    usernames.Add(username);
+}
+
+foreach (string username in usernames) 
+{
+    Console.WriteLine(  username);
+}

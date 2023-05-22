06613198087ce50fee1bc418e4ee1974f596f029
Petar Kobakov <pkobakov@gmail.com>
2023-05-22 23:50:06 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Exercise/Sets of Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = sizes[0];
+int m = sizes[1];
+
+HashSet<int> s = new HashSet<int>();
+HashSet<int> t = new HashSet<int>();
+HashSet<int> result = new HashSet<int>();
+
+for (int i = 0; i < n; i++)
+{
+    s.Add(int.Parse(Console.ReadLine()));
+}
+
+for (int i = 0; i < m; i++)
+{
+    t.Add(int.Parse(Console.ReadLine()));
+
+
+}
+
+
+result = s.Intersect(t).ToHashSet();
+
+
+Console.WriteLine( string.Join(" ", result) );

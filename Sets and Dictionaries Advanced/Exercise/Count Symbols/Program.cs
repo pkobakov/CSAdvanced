d45df29dea2e011aea5fccd1419c057f95fd6359
Petar Kobakov <pkobakov@gmail.com>
2023-05-23 15:40:07 +0300

    Added New Solution

---
+++ Sets and Dictionaries Advanced/Exercise/Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿char[] input = Console.ReadLine().ToCharArray();
+
+Dictionary<char, int> myDictionary = new Dictionary<char, int>();
+int counter = 1;
+for (int i = 0; i < input.Length; i++) 
+{
+    char currentSymbol = input[i]; 
+
+    if (!myDictionary.ContainsKey(currentSymbol))
+    {
+        myDictionary.Add(currentSymbol, 1);
+    }
+
+    else 
+    {
+        myDictionary[currentSymbol] ++;
+    
+    }
+
+    
+
+
+}
+
+foreach (char symbol in myDictionary.Keys.OrderBy(x => x)) 
+{
+     Console.WriteLine($"{symbol}: {myDictionary[symbol]} time/s");
+
+}
+

a184d5c41a5231e360316ece7d37f5d97993f189
Petar Kobakov <pkobakov@gmail.com>
2023-05-25 16:45:55 +0300

    Added New Solution

---
+++ StreamsFilesDirectories/Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,33 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string filesPath = @"../../../Files/TestFolder";
+            string outputPath = @"../../../output.txt";
+
+           GetFolderSize(filesPath, outputPath);
+
+        }
+
+        public static void GetFolderSize(string filesPath, string outputPath)
+        {
+            double sum = 0; 
+            DirectoryInfo dir = new DirectoryInfo(filesPath);
+            FileInfo[] files = dir.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (FileInfo file in files) 
+            {
+                sum += file.Length;
+            }
+
+            sum = sum / 1024.00;
+
+            File.WriteAllText(outputPath, $"{sum.ToString()} KB");
+        }
+    }
+
+}

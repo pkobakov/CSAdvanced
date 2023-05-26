81e994733d96ae8e5eac9ebf135d65228cdf7840
Petar Kobakov <pkobakov@gmail.com>
2023-05-26 23:51:04 +0300

    Added New Solution

---
+++ StreamsFilesDirectories/Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            Directory.CreateDirectory(outputPath);
+            var files = Directory.GetFiles(inputPath);
+            
+            foreach (var file in files) 
+            { 
+                File.Copy(file, Path.Combine(outputPath, Path.GetFileName(file)));
+            }
+
+            foreach (var directory in Directory.GetDirectories(inputPath)) 
+            {
+                CopyAllFiles(directory, Path.Combine(outputPath, Path.GetFileName(directory)));
+            }
+        }
+    }
+}

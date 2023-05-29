03560935699d747b35ef30f817e7ee7a93c21397
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 18:20:52 +0300

    Added New Solution

---
+++ Functional Programming/Lab/Filter by Age/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+Func<string, List<string>> ReadPeople = text => text.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+Dictionary<string,int> persons = new Dictionary<string,int>();  
+for (int i = 0; i < n; i++)
+{
+    List<string> person = ReadPeople(Console.ReadLine());  
+    string name = person[0];
+    int age = int.Parse(person[1]);
+
+    persons.Add(name, age);
+
+
+    
+
+}
+
+Console.WriteLine(  );

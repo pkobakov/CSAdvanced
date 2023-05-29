0fa43c8c929e40dac08856e4b2820c9aed57ac21
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 22:40:17 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Knights of Honor/Program.cs
@@ -0,0 +1,13 @@
+﻿string input = Console.ReadLine();
+Action<string> print =  x => Print(input);
+print(input);
+
+
+void Print(string text) 
+{
+    string [] result = text.Split().Select(x => "Sir " + x).ToArray();
+    Console.WriteLine(string.Join(Environment.NewLine, result));
+
+}
+
+

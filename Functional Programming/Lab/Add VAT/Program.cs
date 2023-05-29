d3729cbb16355c4a4d1db852ff43a4132e7f0b14
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 17:37:47 +0300

    Added New Solution

---
+++ Functional Programming/Lab/Add VAT/Program.cs
@@ -0,0 +1,13 @@
+﻿double [] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+Func<double, double> calculateVAT = a => a * 1.2;
+
+foreach (var num in input)
+{
+    Console.WriteLine($"{calculateVAT(num):f2}");
+}
+
+
+
+
+

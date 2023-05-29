ef8c18babf49003a4507d107ee591af0a676a7aa
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 17:20:33 +0300

    Added New Solution

---
+++ Functional Programming/Lab/Count Uppercase Words/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+string input = Console.ReadLine();
+
+Console.WriteLine(PrintUppercaseWords(input));
+
+static string PrintUppercaseWords(string text) 
+{
+    var test = text.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+    var result = new List<string>();
+    Func<string, bool> check = a => a.Any(x => char.IsUpper(x));
+    foreach (var word in test) 
+    {
+        if (check(word))
+        {
+            result.Add(word);
+        }
+    }
+    return $"{string.Join(Environment.NewLine, result)}";
+}

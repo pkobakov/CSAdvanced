280ca6646982af1b337d1e0db1f4222b2593477d
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 16:09:32 +0300

    Added New Solution

---
+++ Functional Programming/Lab/Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+
+int count = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).Count();
+
+int sum = input.Split(", ")
+                .Select(int.Parse)
+                .Sum();
+
+
+Console.WriteLine(count);
+Console.WriteLine(sum);
+                
+                  
+
+

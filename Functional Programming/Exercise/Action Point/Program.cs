79630b31c69f7973c7ab748937d67c83bc244dae
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 22:29:18 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+
+
+Action<string> action = text =>
+{
+    string [] array = text.Split().ToArray();
+    foreach (var item in array)
+    {
+
+        Console.WriteLine(item);
+    }
+
+};
+
+action(input);
+
+
+
+

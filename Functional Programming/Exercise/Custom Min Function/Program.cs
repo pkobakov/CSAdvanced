62013fc9cc32b3ce326d8d782d1ac1728d1d70c7
Petar Kobakov <pkobakov@gmail.com>
2023-05-29 23:22:31 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Custom Min Function/Program.cs
@@ -0,0 +1,11 @@
+﻿string input = Console.ReadLine();
+
+Func<string, int> min = x => MinNumber(input);
+int result = min(input);
+Console.WriteLine(result);
+
+int MinNumber(string text) 
+{
+    int[] arr = text.Split().Select(int.Parse).OrderBy(x => x).ToArray();
+    return arr[0];
+}

ca0c0c24875d55c2af64ea5d5efb5d84e90d242e
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 15:04:24 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Predicate for Names/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<string> names = Console.ReadLine().Split().ToList();
+
+Predicate<string> checker = x => x.Length <= n;
+
+foreach (var name in names.Where(n => checker(n)))
+{
+    Console.WriteLine( name);
+}

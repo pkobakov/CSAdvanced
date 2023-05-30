1202c5929260f45cd8c464faa0be24662fb4d915
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 17:33:12 +0300

    Added New Solution

--- Functional Programming/Exercise/TriFunction/Program.cs
+++ Functional Programming/Exercise/TriFunction/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+var names = Console.ReadLine()
+     .Split()
+     .ToList();
+
+Func<string, int, bool> checker = (name, value) =>
+ name.ToCharArray()
+.Select(ch => (int)ch).Sum() >= value;
+
+Func<List<string>, int, Func<string, int, bool>, string> namesChecker =
+    (names, n, checker) =>
+ names.FirstOrDefault(name => checker(name, n));
+
+Console.WriteLine(namesChecker(names, n, checker).ToString());
+                  
+        

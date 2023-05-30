70aba1a2fb1eb2c483c9db84e05e4efec35e9f06
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 15:55:11 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/List Of Predicates/Program.cs
@@ -0,0 +1,31 @@
+﻿List<Predicate<int>> predicates = new List<Predicate<int>>();
+int range = int.Parse(Console.ReadLine());
+HashSet<int> deviders = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToHashSet(); 
+
+int[] numbers = Enumerable.Range(1, range).ToArray();
+
+foreach (int devider in deviders) 
+{
+    predicates.Add(x => x%devider == 0);
+}
+
+foreach (int number in numbers) 
+{
+    bool isDivisible = true;
+
+    foreach (var currentPredicate in predicates)
+    {
+        if (!currentPredicate(number))
+        {
+            isDivisible = false;break;
+        }
+    }
+
+    if (isDivisible) 
+    {
+        Console.WriteLine(number);
+    }
+}

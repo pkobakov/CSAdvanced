9405df606f8de1aaeeabbe4a69162c450be12684
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 23:56:07 +0300

    Update Solution.(Special Cars)

--- Functional Programming/Defining Classes/Lab/Car/Car.cs
+++ Functional Programming/Defining Classes/Lab/Car/Car.cs
@@ -16,7 +16,7 @@ namespace CarManufacturer
         private double fuelQuantity;
         private double fuelConsumption;
         private Engine engine;
-        private Tire[] tires; 
+        private Tire[] tires;
 
         public Car()
         {
@@ -28,22 +28,22 @@ namespace CarManufacturer
         }
 
         public Car(string make, string model, int year)
-            :this()
+            : this()
         {
-            this.Make= make;
+            this.Make = make;
             this.Model = model;
             this.Year = year;
         }
 
         public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
-            :this(make, model, year)
+            : this(make, model, year)
         {
-            this.FuelQuantity= fuelQuantity;
-            this.FuelConsumption= fuelConsumption;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
         }
 
         public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
-            :this(make, model, year, fuelQuantity, fuelConsumption)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
         {
             this.Engine = engine;
             this.Tires = tires;
@@ -52,33 +52,31 @@ namespace CarManufacturer
         public string Make { get { return make; } set { make = value; } }
         public string Model { get { return model; } set { model = value; } }
         public int Year { get { return year; } set { year = value; } }
-        public double FuelQuantity { get { return fuelQuantity;  } set { fuelQuantity = value; } }
-        public double FuelConsumption { get {  return fuelConsumption; } set { fuelConsumption = value; } }
+        public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
+        public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
         public Engine Engine { get { return engine; } set { engine = value; } }
         public Tire[] Tires { get { return tires; } set { tires = value; } }
 
-        public void Drive(double distance) 
+        public double Drive(int distance)
         {
-            if (FuelQuantity - (distance * FuelConsumption) > 0) 
+            double neededFuel = this.FuelConsumption * distance / 100;
+            if (this.FuelQuantity >= neededFuel)
             {
-                FuelQuantity -= distance * FuelConsumption;
+                this.FuelQuantity -= neededFuel;
             }
-
-            else
-            {
-                Console.WriteLine("Not enough fuel to perform this trip!");
-            }
+            return this.FuelQuantity;
         }
-
         public string WhoAmI()
         {
             var sb = new StringBuilder();
             sb.AppendLine($"Make: {this.Make}");
             sb.AppendLine($"Model: {this.Model}");
             sb.AppendLine($"Year: {this.Year}");
-            sb.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+            sb.AppendLine($"HorsePowers: {this.Engine.HorsePower}");
+            sb.Append($"FuelQuantity: {this.FuelQuantity}");
 
-           return sb.ToString().TrimEnd();
+            return sb.ToString();
+
         }
     }
 }

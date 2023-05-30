51d5d098a53c22e9c810304d51b82c8456561d76
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 14:45:48 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Reverse and Exclude/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+int number = int.Parse( Console.ReadLine());
+
+Func<List<int>, int, List<int>> action = (x, y) => Reverse(x,y) ;
+
+Console.WriteLine( string.Join(" ", action(numbers, number)));
+
+
+List<int> Reverse(List<int> list, int n) 
+{
+   Predicate<int> checker = x => x % n == 0;
+   return list.Where(x => !checker(x) ).Reverse().ToList();
+}
+

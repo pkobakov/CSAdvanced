c79f5d7e7c5f03fcd1ff9b139c03de43dd71c702
Petar Kobakov <pkobakov@gmail.com>
2023-05-30 14:44:57 +0300

    Added New Solution

---
+++ Functional Programming/Exercise/Applied Arithmetics/Program.cs
@@ -0,0 +1,25 @@
+﻿var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+Action<List<int>> print = n => Console.WriteLine(string.Join(" ", n));
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command == "print")
+        print(numbers);
+    else
+    {
+        Func<int, int> processor = command switch
+        {
+            "add" => n => n + 1,
+            "multiply" => n => n * 2,
+            "subtract" => n => n - 1,
+            _ => n => n
+        };
+        numbers = numbers.Select(processor).ToList();
+
+    }
+}

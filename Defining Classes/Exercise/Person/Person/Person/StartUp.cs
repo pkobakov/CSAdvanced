afe8257c2adf3d1820be2e4f14b16b545b10e9e6
Petar Kobakov <pkobakov@gmail.com>
2023-05-31 21:44:12 +0300

    Added New Solution

---
+++ Defining Classes/Exercise/Person/Person/Person/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace Person
+{
+    using DefiningClasses;
+    using System;
+    public class StartUp
+    {
+		public static void Main(string[] args)
+		{
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personArgs = Console.ReadLine().Split().ToArray();
+
+                Person person = new Person(personArgs[0], int.Parse(personArgs[1]));
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+	}
+}

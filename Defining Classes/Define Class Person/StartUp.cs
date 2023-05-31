72b8802e98828963b518106b6f0e0f76d19d7240
Petar Kobakov <pkobakov@gmail.com>
2023-05-31 14:15:45 +0300

    Added New Solution

---
+++ Defining Classes/Define Class Person/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses
+{
+	using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			
+
+			Person person1 = new Person("Peter", 20);
+			Person person2 = new Person("George", 18);
+			Person person3 = new Person("Jose", 43);
+		}
+	}
+}

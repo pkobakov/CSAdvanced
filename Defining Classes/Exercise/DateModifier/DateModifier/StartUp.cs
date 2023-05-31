8ed325fa331bc496fb34842e1758429f1a225c0c
Petar Kobakov <pkobakov@gmail.com>
2023-05-31 22:45:18 +0300

    Added New Solution

---
+++ Defining Classes/Exercise/DateModifier/DateModifier/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DateModifier
+{
+	using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+
+			DateModifier modifier = new DateModifier();
+			List<string> dates = new List<string> { };
+			
+			for (int i = 0; i < 2; i++)
+			{
+		     	string command = Console.ReadLine();
+				dates.Add(command);
+			}
+
+			Console.WriteLine(modifier.CalculateDifference(dates[0], dates[1]));
+        }
+	}
+}

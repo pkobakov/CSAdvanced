eb6b3a5c2fc155e7e1452b5c7d6a0ed1de4445ff
Petar Kobakov <pkobakov@gmail.com>
2023-05-31 20:56:37 +0300

    Added Folder

---
+++ Defining Classes/Exercise/DefiningPerson/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+	using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+
+
+			int n = int.Parse(Console.ReadLine());
+
+			Family family = new Family();
+
+			for (int i = 0; i < n; i++)
+			{
+				string [] personArgs = Console.ReadLine().Split().ToArray();
+
+				Person person = new Person(personArgs[0], int.Parse(personArgs[1]));
+				family.AddMember(person);
+			}
+
+			Person oldest = family.GetOldestMember();
+			Console.WriteLine($"{oldest.Name} {oldest.Age}");
+
+          
+        }
+	}
+}

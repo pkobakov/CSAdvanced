3eaa90e942fff479347ec94ef2431875ea6bd317
Petar Kobakov <pkobakov@gmail.com>
2023-06-01 17:09:01 +0300

    change Folder Name

---
+++ Defining Classes/Exercise/Defining Classes/Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{ 
+    using System;
+    public class StartUp
+    {
+		public static void Main(string[] args)
+		{
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personArgs = Console.ReadLine().Split().ToArray();
+
+                Person person = new Person(personArgs[0], int.Parse(personArgs[1]));
+                family.AddMember(person);
+            }
+
+            foreach (var person in family.People.Where(p => p.Age > 30).OrderBy(p => p.Name)) 
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+	}
+}
---
+++ Defining Classes/Exercise/Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer) 
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = 0;
+
+        }
+        
+        public string Model { get { return model; } private set { model = value; } }   
+        public double FuelAmount { get { return fuelAmount; } private set { fuelAmount = value; } }
+        public double FuelConsumptionPerKilometer { get { return fuelConsumptionPerKilometer; } private set { fuelConsumptionPerKilometer = value; } }
+        public double TravelledDistance { get; private set; }
+
+        public void Drive(string model , int amountOfKm)
+        {  
+            double fuelNeeded =  FuelConsumptionPerKilometer * amountOfKm;
+           if (fuelNeeded <= this.FuelAmount)
+           {
+                this.TravelledDistance += amountOfKm;
+                this.FuelAmount -= fuelNeeded;
+           }
+
+           else 
+           {
+                Console.WriteLine("Insufficient fuel for the drive");
+           }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:f2} {this.TravelledDistance}";
+        }
+    }
+}

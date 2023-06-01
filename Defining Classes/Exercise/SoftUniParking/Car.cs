c91197a3438850f118808f26b836c127a7435f86
Petar Kobakov <pkobakov@gmail.com>
2023-06-01 23:20:03 +0300

    Added New Solution

---
+++ Defining Classes/Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        //Make: string
+        //Model: string
+        //HorsePower: int
+        //RegistrationNumber: string
+
+        private string make; 
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+               this.Make = make;
+               this.Model = model;
+               this.HorsePower = horsePower;
+               this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int HorsePower { get { return horsePower;  } set { horsePower = value; } }
+        public string RegistrationNumber { get { return registrationNumber; } set { registrationNumber = value; } }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();   
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"HorsePower: {this.HorsePower}");
+            sb.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Defining Classes/Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+        private int count;
+        public Parking(int capacity) 
+        {
+           this.cars = new List<Car>();
+           this.Capacity = capacity;
+
+        }
+
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get => this.cars.Count; }
+
+        public string AddCar(Car car) 
+        {
+            if (this.cars.Any(c => c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+
+
+            if (this.capacity <= this.cars.Count)
+            {
+                return "Parking is full!";
+            }
+
+            this.cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            
+        }
+
+        public string RemoveCar(string RegistrationNumber) 
+        {
+            if (!this.cars.Any(c => c.RegistrationNumber == RegistrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            else 
+            {
+               Car car = this.cars.Find(c => c.RegistrationNumber == RegistrationNumber);
+               this.cars.Remove(car); 
+               return $"Successfully removed {car.RegistrationNumber}";
+            }
+        }
+
+        public Car GetCar(string RegistrationNumber) 
+        {
+            Car car = this.cars.FirstOrDefault(c => c.RegistrationNumber == RegistrationNumber);
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers) 
+        {
+            foreach (string regNum in registrationNumbers)
+            {
+                while (this.cars.Any(c => c.RegistrationNumber == regNum))
+                {
+                    Car currCar = this.cars.FirstOrDefault(c => c.RegistrationNumber == regNum);
+                    this.cars.Remove(currCar);
+                }
+            }
+
+        }
+    }
+}

0811822bb218a75d51ad62ed7c5257d6207d7130
Petar Kobakov <pkobakov@gmail.com>
2023-06-01 17:03:27 +0300

    Added New Solution

---
+++ Defining Classes/Exercise/Person/RawData/Utilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawData.Utilities
+{
+    public static class Constants
+    {
+        public const string fragile = "fragile";
+        public const string flammable = "flammable";
+    }
+}

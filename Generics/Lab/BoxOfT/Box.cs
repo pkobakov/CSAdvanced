a4a12152b68569eb8089242650477454cb2204b4
Petar Kobakov <pkobakov@gmail.com>
2023-06-06 20:26:31 +0300

    Add New Solution

---
+++ Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box <T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count { get { return list.Count; } }
+        public void Add(T item) 
+        {
+            list.Add(item);
+        }
+
+        public T Remove() 
+        {
+           var item = list[Count-1];
+           list.Remove(item);
+           return item;
+        }
+    }
+}

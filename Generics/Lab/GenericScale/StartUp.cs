03908c1e774f89f0efd3f312363d9302d80472a0
Petar Kobakov <pkobakov@gmail.com>
2023-06-06 22:36:28 +0300

    Added New Solution

---
+++ Generics/Lab/GenericScale/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericScale
+{
+	using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int a = 1;
+			int b = 2;
+
+
+			EqualityScale<int> equality = new EqualityScale<int>(a, b);
+
+			Console.WriteLine(equality.AreEqual());
+
+		}
+	}
+}

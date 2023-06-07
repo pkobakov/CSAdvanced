6fb8fe739a0dfdd5b3e563f09a1dae489cf2f3b3
Petar Kobakov <pkobakov@gmail.com>
2023-06-07 15:36:13 +0300

    Added New Solution

---
+++ Generics/Exercise/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericSwapMethodStrings
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+	        SwapStrings<string> swapStrings = new SwapStrings<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				swapStrings.Add(Console.ReadLine());
+			}
+
+			int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			swapStrings.Swap(indexes[0], indexes[1]);
+
+			Console.WriteLine(swapStrings.ToString());
+		}
+	}
+}
---
+++ Generics/Exercise/GenericSwapMethodStrings/SwapStrings.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericSwapMethodStrings
+{
+    using System.Text;
+
+    public class SwapStrings <T> 
+	{ 
+	    private List<T> list;
+        public SwapStrings()
+        {
+            this.list = new List<T>();
+        }
+
+		public void Add(T item) 
+		{
+		    list.Add(item);
+		
+		}
+
+		public void Swap (int indexA, int indexB)
+		{
+			T swap = list[indexA];
+			list[indexA] = list[indexB];
+			list[indexB] = swap;
+		}
+
+        public override string ToString()
+        {
+			StringBuilder sb = new StringBuilder();
+			foreach (T item in list) 
+			{
+			    sb.AppendLine($"{typeof(T)}: {item}");
+			
+			}
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

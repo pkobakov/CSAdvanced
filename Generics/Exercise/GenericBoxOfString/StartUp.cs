700c6841f03bdf2c7893dc496e6b40c594b5e1f5
Petar Kobakov <pkobakov@gmail.com>
2023-06-07 08:44:23 +0300

    Added New Solution

---
+++ Generics/Exercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericBoxOfString
+{
+	using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+			   string s = Console.ReadLine();
+			   Box<string> box = new Box<string>(s);
+               Console.WriteLine(box);
+
+			}
+        }
+	}
+}

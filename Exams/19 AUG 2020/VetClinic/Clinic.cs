fb4e080134569110f09e0edd8f5af3fc59325233
Petar Kobakov <pkobakov@gmail.com>
2023-06-09 10:45:51 +0300

    Added New Solution

---
+++ Exams/19 AUG 2020/VetClinic/Clinic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private int capacity;
+        private List<Pet> data;
+
+        public Clinic( int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return data.Count; } } 
+
+        public void Add(Pet pet) 
+        {
+            if (this.Capacity > this.Count)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove (string name)
+        {
+            if (this.data.Any())
+            {
+                Pet pet = this.data.FirstOrDefault(p => p.Name == name);
+                this.data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner) 
+        {
+            if (this.data.Any())
+            {
+               return  this.data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+            }
+
+            return null;
+        }
+
+        public Pet GetOldestPet() 
+        {
+            if (this.data.Any())
+            {
+                return this.data.OrderByDescending(p => p.Age).FirstOrDefault();
+            }
+
+            return null;
+        }
+
+        public string GetStatistics() 
+        {
+            if (!this.data.Any())
+            {
+                return null; 
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (Pet pet in this.data) 
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString();   
+        }
+
+
+    }
+}
---
+++ Exams/19 AUG 2020/VetClinic/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        private string name;
+        private int age;
+        private string owner;
+
+        public Pet(string name,  int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age; 
+            this.Owner = owner; 
+        }
+
+        public  string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string Owner { get { return owner; } set { owner = value; } }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}";
+        }
+    }
+}

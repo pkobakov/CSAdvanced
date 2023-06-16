3daed613839886b75f74ea35866dcb41f118c311
Petar Kobakov <pkobakov@gmail.com>
2023-06-16 23:15:54 +0300

    Added New Solution

---
+++ Exams/18 February 2023/SoftUniKindergarten/Child.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniKindergarten
+{
+    public class Child
+    {
+        private string firstName;
+        private string lastName;    
+        private int age;
+        private string parentName;
+        private string contactNumber;
+
+        public Child(string firstName, string lastName, int age, string parentName, string contactNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.ParentName = parentName;
+            this.ContactNumber = contactNumber;
+        }
+
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string ParentName { get { return parentName; } set { parentName = value; } }
+        public string ContactNumber { get { return contactNumber; } set { contactNumber = value; } }
+
+        public override string ToString() 
+        { 
+            return $"Child: {this.FirstName} {this.LastName}, Age: {this.Age}, Contact info: {this.ParentName} - {this.ContactNumber}";
+        }
+    }
+}
---
+++ Exams/18 February 2023/SoftUniKindergarten/Kindergarten.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        //private string name;
+        //private int capacity;
+        //private List<Child> registry;
+        public Kindergarten(string name, int capacity) 
+        {
+           this.Name = name;
+           this.Capacity = capacity;
+           this.Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public bool AddChild(Child child) 
+        {
+            if (this.Registry.Count >= this.Capacity)
+            {
+                return false;
+            }
+
+            this.Registry.Add(child);
+            return true;
+        
+        }
+
+        public int ChildrenCount { get { return this.Registry.Count; } }
+
+        public bool RemoveChild(string name) 
+        {
+            string[] fullname = name.Split().ToArray();
+
+            string firstName = fullname[0];
+            string lastName = fullname[1];
+
+            var child = this.Registry.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (child != null) 
+            {
+                this.Registry.Remove(child);
+                return true;
+            }
+
+            return false;
+        }
+        public Child GetChild(string childFullName) 
+        {
+            string[] fullname = childFullName.Split().ToArray();
+
+            string firstName = fullname[0];
+            string lastName = fullname[1];
+
+            var child = this.Registry.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (child == null) { return null; }
+
+            return child;
+
+        }
+
+        public string RegistryReport() 
+        {
+           StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Registered children in {this.Name}:");
+
+            foreach (var child in this.Registry.OrderByDescending(x => x.Age)
+                                               .ThenBy(x => x.LastName)
+                                               .ThenBy(x => x.FirstName))
+            {
+                stringBuilder.AppendLine(child.ToString());
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+
+    }
+}

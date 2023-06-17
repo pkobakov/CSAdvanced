092c7ad666d9b834ca53e068caf6b5b5798083e6
Petar Kobakov <pkobakov@gmail.com>
2023-06-17 13:03:02 +0300

    Added New Solution

---
+++ Exams/Final Exam/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+
+        public RepairShop(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Vehicles = new List<Vehicle>();
+        }
+        public int Capacity { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (this.Vehicles.Count < this.Capacity)
+            {
+                this.Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            var vehicle = this.Vehicles.FirstOrDefault(v => v.VIN == vin);
+            if (vehicle == null)
+            {
+                return false;
+
+            }
+            this.Vehicles.Remove(vehicle);
+            return true;
+        }
+
+        public int GetCount() => this.Vehicles.Count;
+        public Vehicle GetLowestMileage()
+        {
+            return this.Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("Vehicles in the preparatory:");
+
+            foreach (var vehicle in this.Vehicles)
+            {
+
+                stringBuilder.AppendLine(vehicle.ToString());
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+
+        }
+    }
+}
---
+++ Exams/Final Exam/AutomotiveRepairShop/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+        private string vin;
+        private int mileage;
+        private string damage;
+
+        public Vehicle(string vin, int mileage, string damage)
+        {
+            this.VIN = vin;
+            this.Mileage = mileage;
+            this.Damage = damage;
+        }
+        public string VIN { get { return vin; } set { vin = value; } }
+        public int Mileage { get { return mileage; } set { mileage = value; } }
+        public string Damage { get { return damage; } set { damage = value; } }
+
+
+        public override string ToString()
+        {
+            return $"Damage: {this.Damage}, Vehicle: {this.VIN} ({this.Mileage} km)";
+        }
+    }
+}
